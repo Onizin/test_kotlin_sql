9fa32594dbd571d4a1dd857e58fdf1ec8dadae68
dika <wold.raqi@gmail.com>
2025-07-10 10:41:42 +0700

    type data.

--- app/src/main/java/com/example/test_kotlin_sql/MainActivity.kt
+++ app/src/main/java/com/example/test_kotlin_sql/MainActivity.kt
@@ -7,14 +7,37 @@ import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+
+    var angkaBulat: Int = 0
+    var angkaDesimal: Double = 0.0
+    var angkaPanjang: Long = 900000000000L
+
+    var karakter: Char = 'A'
+    var kata: String = "Hello, World!"
+
+    var benar: Boolean = true
+    var salah: Boolean = false
+
+    var angkaArray: Array<Int> = arrayOf(1, 2, 3, 4, 5)
+    var stringArray: Array<String> = arrayOf("Kotlin", "Java", "Python")
+    var booleanArray: Array<Boolean> = arrayOf(true, false, true)
+
+    fun hitung(angka: Int, kata: String) {
+
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        hitung(10, "Kotlin")
+
+        if(angkaBulat==4 && angkaDesimal>3.14){
+            benar = true
+            kata = "Angka Bulat adalah 4"
+        } else {
+            benar = false
+
         }
     }
 }
